71d52c5cdfe69a8a0e384cb2b8bcc0b93732cd88
yrustamjon <yolr11861@gmail.com>
2025-07-25 09:19:45 +0500

    frontga faqat activ banner yuborish qoshildi

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -5,3 +5,6 @@ from ..projects.serializers import BannerSerializer
 class BannerListView(generics.ListAPIView):
     queryset = Banner.objects.all()
     serializer_class = BannerSerializer
+    
+    def get_queryset(self):
+        return self.queryset.filter(is_active=True)
