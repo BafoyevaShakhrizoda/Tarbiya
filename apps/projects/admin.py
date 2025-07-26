d0cd2a1822df4cefe4339334fec62c9e076c6b5f
Bakhtigul <baxtigul1905@mail.ru>
2025-07-26 17:34:35 +0500

    search

--- apps/projects/admin.py
+++ apps/projects/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Banner, Videos, Music, Books
+from .models import Banner, Videos, Music, Books,TextBooks
 
 admin.site.register(Banner)
 admin.site.register(Videos)
 admin.site.register(Music)
 admin.site.register(Books)
+admin.site.register(TextBooks)
